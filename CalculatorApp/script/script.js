1ed9c94aaa63ef342b68a7e02d4106b88699e79c
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-07-07 21:20:51 +0700

    Update Project Structure

--- CalculatorApp/script/script.js
+++ CalculatorApp/script/script.js
@@ -1,5 +1,5 @@
 var arrayOfNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var arrayOfOpe = ['+', '-', '/', '*', '^', 	'\u221A'];
+var arrayOfOpe = ['+', '-', '/', '*', '^', '\u221A'];
 
 function rFact(num){
 	if (num === 0){ 
