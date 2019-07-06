c1521a832a2a98ed1bc9577dd2b9c082e2cdb65e
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-07-06 22:45:59 +0900

    [Test] read data from json file

--- blog/js/gen-posts-list.js
+++ blog/js/gen-posts-list.js
@@ -1,6 +1,25 @@
 function genPostsPreviewFromSource(fileURL) {
     $.getJSON(fileURL, function (data) {
-        console.log(data);
+        var postsList = [];
+        $.each(data, function (key, val){
+            postsList.push(
+                '<div class="post-preview">' +
+                    `<a href="https://tuananhhedspibk.github.io/blog/posts/${key}.html">` +
+                        '<h2 class="post-title">' +
+                            val['title'] +
+                        '</h2>' +
+                        '<h3 class="post-subtitle">' +
+                            + val['sub_title'] +
+                        '</h3>' +
+                    '</a>' +
+                    '<p class="post-meta">' + val['posted_at'] + '</p>' +
+                '</div>' +
+                '<hr>'
+            );
+        });
+        $('<div/>', {
+            html: postsList.join('')
+        }).appendTo('#posts-list');
     });
 }
 
