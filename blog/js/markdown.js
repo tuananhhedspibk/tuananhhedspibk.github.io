f156b7c853fce09758e4bbefd8ba933bea0fdde6
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-07-06 17:11:33 +0900

    Update for readable code chapter 2, 3, 4

---
+++ blog/js/markdown.js
@@ -0,0 +1,22 @@
+function highlightCode() {
+    var pres = document.querySelectorAll("pre>code");
+    for (var i = 0; i < pres.length; i++) {
+      hljs.highlightBlock(pres[i]);
+    }
+}
+
+function showMarkdownFromFileAt(fileURL) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open('GET', fileURL, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+            var allText = rawFile.responseText;
+            var converter = new showdown.Converter();
+            document.getElementById('content').innerHTML = converter.makeHtml(allText);
+            highlightCode();
+            }
+        }
+    }
+    rawFile.send(null);
+}
