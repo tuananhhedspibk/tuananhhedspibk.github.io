9ed9b47e57d6e5c7183d9ea771da459fa9e5dc09
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2021-04-29 12:24:25 +0900

    Add quote display feature

--- blog/js/quote-display.js
+++ blog/js/quote-display.js
@@ -4,7 +4,7 @@ rawFile.onreadystatechange = function () {
   if(rawFile.readyState === 4) {
     if (rawFile.status === 200 || rawFile.status == 0) {
       var data = rawFile.responseText;
-      console.log(data);
+      console.log(data["quotes"]);
     }
   }
 }
