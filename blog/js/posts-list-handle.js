0527c4e2b8559ec9100798de9c83dd95668772b3
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2021-09-23 22:06:06 +0900

    Update status icon

--- blog/js/posts-list-handle.js
+++ blog/js/posts-list-handle.js
@@ -1,93 +1,120 @@
 var numberPosts = 0;
 var lastDisplayElementIndex = 0;
-var nonTechTagValues = ['Translation', 'Experience', 'Tip & Trick', 'Summary', 'Grammar', 'Note'];
-var languagesTagValues = ['english', 'japanese', 'french', 'vietnamese', 'vietnamese2'];
+var nonTechTagValues = [
+  "Translation",
+  "Experience",
+  "Tip & Trick",
+  "Summary",
+  "Grammar",
+  "Note",
+];
+var languagesTagValues = [
+  "english",
+  "japanese",
+  "vietnamese",
+  "vietnamese2",
+];
 var rootBaseURL = "https://tuananhhedspibk.github.io/blog";
 
 function genPostsPreviewFromSource(fileURL) {
-    $.getJSON(fileURL, function (data) {
-        var postsList = [];
-        var counter = 0;
-        $.each(data, function (key, val){
-            var representClassName = counter <= 7 ? '' : ' hide-element';
-            var valuesOfTags = val['tags'];
-            var tagElements = [];
-            valuesOfTags.forEach(tagValue => {
-                var classOfTag = 'tech-tag';
+  $.getJSON(fileURL, function (data) {
+    var postsList = [];
+    var counter = 0;
+    $.each(data, function (key, val) {
+      console.log("val", val);
 
-                if (languagesTagValues.indexOf(tagValue) !== -1) {
-                    let flagCSSClass = '';
+      var representClassName = counter <= 7 ? "" : " hide-element";
+      var valuesOfTags = val["tags"];
+      var tagElements = [];
+      var statusIcon;
+      valuesOfTags.forEach((tagValue) => {
+        var classOfTag = "tech-tag";
 
-                    if (tagValue == 'japanese') {
-                        flagCSSClass = ' border-flag';
-                    }
+        if (languagesTagValues.indexOf(tagValue) !== -1) {
+          let flagCSSClass = "";
 
-                    tagElements.push(`
-                        <img class="flag${flagCSSClass}"
-                            src="${rootBaseURL + '/img/' + tagValue}.svg"></img>
-                    `);
-                    return;
-                }
-                if (nonTechTagValues.indexOf(tagValue) !== -1) {
-                    classOfTag = 'non-tech-tag';
-                }
-                tagElements.push(`
-                    <a class="${classOfTag}">${tagValue}</a>
-                `);
-            });
-            lastDisplayElementIndex = counter <= 4 ? counter : lastDisplayElementIndex;
-            postsList.push(
-                `<div class="post-preview${representClassName}" id="post_${counter}">` +
-                    `<a href="https://tuananhhedspibk.github.io/blog/posts/${key}.html">` +
-                        '<h2 class="post-title">' +
-                            val['title'] +
-                        '</h2>' +
-                        '<h3 class="post-subtitle">' +
-                            val['description'] +
-                        '</h3>' +
-                    '</a>' +
-                    '<div class="tags">' + tagElements.join('') + '</div>' +
-                    '<p class="post-meta">' + val['posted_at'] + '</p>' +
-                '</div>' +
-                `<hr id="hr_${counter}" class="${representClassName}">`
-            );
-            counter++;
-        });
-        numberPosts = counter;
-        postsList.push(
-            '<div class="clearfix">' +
-                '<a class="btn btn-primary float-right"' +
-                ' id="older_posts_display_btn" onClick="displayOlderPosts()">Older Posts &rarr;</a>' +
-            '</div>'
-        );
+          if (tagValue == "japanese") {
+            flagCSSClass = " border-flag";
+          }
 
-        $('<div/>', {
-            html: postsList.join('')
-        }).appendTo('#posts-list');
+          tagElements.push(`
+            <img class="flag${flagCSSClass}"
+                src="${rootBaseURL + "/img/" + tagValue}.svg"></img>
+          `);
+          return;
+        }
+        if (nonTechTagValues.indexOf(tagValue) !== -1) {
+          classOfTag = "non-tech-tag";
+        }
+        tagElements.push(`
+          <a class="${classOfTag}">${tagValue}</a>
+        `);
+      });
+      if (val["status"] == "in_progress") {
+        statusIcon = "<i class='fas fa-hourglass-half'></i>";
+      } else if (val["status"] == "done") {
+        statusIcon = "<i class='fas fa-check-circle'></i>";
+      }
+      lastDisplayElementIndex =
+        counter <= 4 ? counter : lastDisplayElementIndex;
+      postsList.push(
+        `<div class="post-preview${representClassName}" id="post_${counter}">` +
+          `<a href="https://tuananhhedspibk.github.io/blog/posts/${key}.html">` +
+          '<div class="post-title-wrapper">' +
+          '<p class="post-title">' +
+          val["title"] +
+          "</p>" +
+          statusIcon +
+          '</div>' +
+          '<h3 class="post-subtitle">' +
+          val["description"] +
+          "</h3>" +
+          "</a>" +
+          '<div class="tags">' +
+          tagElements.join("") +
+          "</div>" +
+          '<p class="post-meta">' +
+          val["posted_at"] +
+          "</p>" +
+          "</div>" +
+          `<hr id="hr_${counter}" class="${representClassName}">`
+      );
+      counter++;
     });
+    numberPosts = counter;
+    postsList.push(
+      '<div class="clearfix">' +
+        '<a class="btn btn-primary float-right"' +
+        ' id="older_posts_display_btn" onClick="displayOlderPosts()">Older Posts &rarr;</a>' +
+        "</div>"
+    );
+
+    $("<div/>", {
+      html: postsList.join(""),
+    }).appendTo("#posts-list");
+  });
 }
 
-genPostsPreviewFromSource('https://tuananhhedspibk.github.io/blog/data/posts_preview.json');
+genPostsPreviewFromSource(
+  "https://tuananhhedspibk.github.io/blog/data/posts_preview.json"
+);
 
-function displayOlderPosts () {
-    // Init variable value
-    var upperBound = 0;
+function displayOlderPosts() {
+  var upperBound = 0;
 
-    if (lastDisplayElementIndex == numberPosts - 1) {
-        return;
-    }
-    else if (lastDisplayElementIndex + 5 < numberPosts) {
-        upperBound = lastDisplayElementIndex + 6;
-    }
-    else {
-        $('#older_posts_display_btn').hide(500);
-        upperBound = numberPosts;
-    }
+  if (lastDisplayElementIndex == numberPosts - 1) {
+    return;
+  } else if (lastDisplayElementIndex + 5 < numberPosts) {
+    upperBound = lastDisplayElementIndex + 6;
+  } else {
+    $("#older_posts_display_btn").hide(500);
+    upperBound = numberPosts;
+  }
 
-    for (var i = lastDisplayElementIndex + 1 ; i < upperBound; i++) {
-        $(`#post_${i}`).removeClass('hide-element', 1000, 'easeOutBounce');
-        $(`#hr_${i}`).removeClass('hide-element', 1000, 'easeOutBounce');
-    }
+  for (var i = lastDisplayElementIndex + 1; i < upperBound; i++) {
+    $(`#post_${i}`).removeClass("hide-element", 1000, "easeOutBounce");
+    $(`#hr_${i}`).removeClass("hide-element", 1000, "easeOutBounce");
+  }
 
-    lastDisplayElementIndex = upperBound - 1;
+  lastDisplayElementIndex = upperBound - 1;
 }
